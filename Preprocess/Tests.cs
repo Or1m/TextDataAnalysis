a923ee8095a8727cf72cbf4339572e685a41e92a
Miro Kačeriak <miro.kaceriak@peruncreative.com>
2023-03-25 10:41:02 +0100

    Pridane testy

---
+++ Preprocess/Tests.cs
@@ -0,0 +1,27 @@
+﻿namespace Preprocess
+{
+    public class Tests
+    {
+        private void A()
+        {
+            //for (int i = 0; i<filteredWords.Count; i++)
+            //{
+            //    var item = filteredWords[i];
+            //    if (item.Length == 1 && !char.IsDigit(item[0]) && item != "i")
+            //    {
+
+            //        Console.WriteLine($"{filteredWords[i - 1]} + {item}");
+            //    }
+            //}
+            //return;
+        }
+        private void B()
+        {
+            //for (int i = 0; i < filteredWords.Count; i++)
+            //{
+            //    if (filteredWords[i].Any(char.IsDigit) && !filteredWords[i].All(char.IsDigit))
+            //        Console.WriteLine(filteredWords[i]);
+            //}
+        }
+    }
+}
